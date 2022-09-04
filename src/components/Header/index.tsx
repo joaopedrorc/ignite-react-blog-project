5df54e2190fda71cb4280bdffe7fab9394db5a07
João Pedro Cordeiro <jpc5626@gmail.com>
2022-09-04 19:56:08 -0300

    feat: Header component and Home Layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a href="/">
+          <img src="/assets/Logo.svg" alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
+
+import CalendarIcon from '../../public/assets/calendar.svg';
+import userIcon from '../../public/assets/user.svg';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,9 +29,66 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const posts = [
+    {
+      uid: 'ere1232',
+      first_publication_date: '15 Mar 2021',
+      data: {
+        title: 'Como utilizar Funções',
+        subtitle: 'Sincronização em vez de ciclos de vida.',
+        autor: 'Joseph Oliveira',
+      },
+    },
+    {
+      uid: 'ere12ww32',
+      first_publication_date: '19 Abr 2021',
+      data: {
+        title: 'Criando um app CRA do zero',
+        subtitle:
+          'Tudo sobre como criar a sua primeira aplicação utilizando Create React App',
+
+        autor: 'Danilo Vieira',
+      },
+    },
+    {
+      uid: 'ere123de2',
+      first_publication_date: '15 Mar 2021',
+      data: {
+        title: 'Como utilizar Hooks',
+        subtitle: 'Pensando em sincronização em vez de ciclos de vida.',
+        autor: 'Joseph Oliveira',
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Home | Spacetraveling Blog</title>
+      </Head>
+
+      <main className={styles.homeContainer}>
+        <article className={styles.homeContent}>
+          {posts.map(item => (
+            <Link key={item.uid} href={`/posts/${item.uid}`}>
+              <a>
+                <header>{item?.data?.title}</header>
+                <section>{item?.data?.subtitle}</section>
+                <footer>
+                  <img src="/assets/calendar.svg" alt="Calendar icon" />
+                  <time>{item?.first_publication_date}</time>
+                  <img src="/assets/user.svg" alt="User icon" />
+                  <div>{item?.data?.autor}</div>
+                </footer>
+              </a>
+            </Link>
+          ))}
+        </article>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
